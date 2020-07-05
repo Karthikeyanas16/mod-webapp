e8b0bc81cdd8e62afd8af4d88216c66ad910f47c
ltiuserjf39@iiht.tech <ltiuserjf39@iiht.tech>
2020-07-05 14:03:57 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './compenents/login/login.component';
+import { RegisterComponent } from './compenents/register/register.component';
+import { ProfileComponent } from './compenents/profile/profile.component';
+import { DetailComponent } from './compenents/detail/detail.component';
+import { HomeComponent } from './compenents/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    DetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
